ff314b346ee30ea64d194735aeb1b083619874e7
Mub1532 <mubkhan644@gmail.com>
2026-09-08 23:23:16 +0100

    rename extension to fit play books branding guidelines and also chrome

--- config/manifest.ts
+++ config/manifest.ts
@@ -1,9 +1,9 @@
 import type { UserManifest } from "wxt";
 
 export default {
-  name: "GP Books Extended",
+  name: "Books Extended: for Google Play Books™",
   description:
-    "Extension to add features to Google Play Books, such as page animations, bubble zoom, book theme etc.",
+    "Extension to add features to Google Play Books™, such as page animations, bubble zoom, book theme etc.",
   version: "0.1",
   permissions: ["storage"],
 } satisfies UserManifest;
